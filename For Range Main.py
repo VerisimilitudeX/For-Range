21954f73762fb3b23ecd41316509a8c44d121f78
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-25 13:09:35 -0700

    Create For Range Main.py

---
+++ For Range Main.py
@@ -0,0 +1,4 @@
+books = ["Good Omens", "Harry Potter Series", "Catch 22", "Enchantress from the Stars", "Cloud Atlas", "Left Hand of Darkness", "Ender's Game", "The Raven Cycle", "The Hunger Games", "Dragonriders of Pern"]
+
+for i in range(2, len(books), 3):
+    print(str(i) + ":" + books[i])
