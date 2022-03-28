f2391f92e00701ac83b634f2f031cc2a6063f8eb
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-28 13:18:58 -0700

    Update New Year Countdown.py

--- New Year Countdown.py
+++ New Year Countdown.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 for i in range(10, 0, -1):
-    print(str(i) + " seconds")
+    print(str(i) + " more seconds until the New Year...")
     pygame.time.wait(1000)
     def clrscr():
         print ("\n" * 50)
