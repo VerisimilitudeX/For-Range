f37500cb1c4cce871fdd49667720424e309e356b
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-28 13:04:56 -0700

    Create New Year Countdown.py

---
+++ New Year Countdown.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+for i in range(10, 0, -1):
+    print(str(i) + " seconds")
+    pygame.time.wait(1000)
+    def clrscr():
+        print ("\n" * 50)
+    clrscr()
+
+print("\o/ Happy New Year! \o/")
