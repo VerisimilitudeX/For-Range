32f942ba74cb8e4533a2bee03c9c6bf99ea06fd3
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-29 13:08:59 -0700

    Create Brighten Up.py

---
+++ Brighten Up.py
@@ -0,0 +1,18 @@
+import random
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([500, 500])
+window.fill((0, 0, 0))
+
+max_circles = random.randint(20, 50)
+color_increment = int(255 / max_circles)
+
+for i in range(max_circles):
+    color = (i * color_increment, i * color_increment, i * color_increment)
+    radius = i * 5
+    point = (random.randint(25, 475), random.randint(25, 475))
+
+    pygame.draw.circle(window, color, point, radius)
+    pygame.display.flip()
+    pygame.time.wait(200)
