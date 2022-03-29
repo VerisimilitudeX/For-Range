89dcf4e8231435ba26460de0c361aff872923419
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-29 13:15:20 -0700

    Update Brighten Up.py

--- Brighten Up.py
+++ Brighten Up.py
@@ -1,18 +1,36 @@
+#### ---- SET UP ---- ####
+
+# --- Libraries --- #
 import random
 import pygame
 pygame.init()
 
+# --- Window --- #
 window = pygame.display.set_mode([500, 500])
 window.fill((0, 0, 0))
 
+# --- Variables --- #
 max_circles = random.randint(20, 50)
 color_increment = int(255 / max_circles)
 
+#### ---- DRAW LOOP ---- ####
+quit = False
 for i in range(max_circles):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit = True
+
     color = (i * color_increment, i * color_increment, i * color_increment)
     radius = i * 5
+
     point = (random.randint(25, 475), random.randint(25, 475))
 
+    # --- Draw circle and pause --- #
     pygame.draw.circle(window, color, point, radius)
     pygame.display.flip()
     pygame.time.wait(200)
+    
+    if quit == True:
+        break
+        
+print(str(max_circles) + " circles")
